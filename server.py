66e922b6da23ed03d7d11d87bb6d98c2d4019ec7
Ensar Gök <ensarxgok@gmail.com>
2023-07-19 12:41:36 +0300

    Update server.py

--- server.py
+++ server.py
@@ -12,8 +12,16 @@ class Server:
         self.allowed_authentication_types = allowed_authentication_types
         self.host = host
         self.port = port
-        self.startup_func = lambda: None
-        self.establish_func = lambda: None
+        self.listen_process_thread = None
+
+        """
+        Events
+        on_startup: called when the server is started
+        on_establish: called when a client is established
+        on_message: called when a client sends a message
+        on_leave: called when a client leaves
+        on_shutdown: called when the server is closed
+        """
 
     def listen_process(self):
         """
@@ -25,7 +33,7 @@ class Server:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((self.host, self.port))
         self.sock.listen(5)
-        while True:
+        while self.is_running:
             print("Waiting for connection...")
             client, address = self.sock.accept()
             thread = threading.Thread(target = self.establish_connection, args = (client,address))
@@ -40,20 +48,23 @@ class Server:
     """
     user can use @app.on_startup to add a function to be called when the server is started, it can be null
     """
-    def on_startup(self, func):
+    def event(self, func: callable):
+        """
+        Event
+        """
         if not callable(func):
             raise TypeError("on_startup function must be callable")
-        self.startup_func = func
-
-    def on_establish(self, func):
-        if not callable(func):
-            raise TypeError("on_establish function must be callable")
-        self.establish_func = func
+  
+        setattr(self, func.__name__, func)
+        return func
 
     def start(self):
-        self.startup_func()
+        self.on_startup()
+        self.is_running = True
         listen_process = threading.Thread(target = self.listen_process)
         listen_process.start()
+        self.listen_process_thread = listen_process
+        return
 
     def establish_connection(self, client_sock, address):
         """
@@ -72,7 +83,7 @@ class Server:
         # assume that the client is a valid ssh client and username is client1_username
         client_obj = Client(self, "client1_username")
         self.clients[client_obj] = client_sock
-        self.establish_func()
+        self.on_establish(client_obj)
         return
 
 
@@ -117,3 +128,35 @@ class Server:
         print(client_obj)
         self.queue.append({client_obj: client})
         print(self.queue)
+
+    def shutdown(self):
+        print("0000")
+        self.on_shutdown()
+        self.is_running = False
+        # close all the connections
+        for client in self.clients:
+            self.clients[client].close()
+        # close the listening_thread
+        print("1 the server...")
+        self.listen_process_thread.join()
+        print("2 the server...")
+        self.sock.close()
+        return
+
+
+    # EVENT HANDLERS
+
+    def on_startup(self):
+        return
+    
+    def on_establish(self):
+        return
+    
+    def on_leave(self):
+        return
+    
+    def on_close(self):
+        return
+    
+    def on_message(self, client, message):
+        return
