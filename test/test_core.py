51e43de4945fc82adc2f4b9dc22521117e0ed698
Ensar Gök <ensarxgok@gmail.com>
2023-11-13 23:32:23 +0300

    more rfc added to core

--- test/test_core.py
+++ test/test_core.py
@@ -5,7 +5,7 @@ from pyssh._core import _conn_setup, _core_classes
 # TEST _conn_setup_version_exchange
 
 class test__client_base(unittest.TestCase):
-    def test__conn_setup_version_exchange(self):        
+    def test__protocol_version_exchange(self):
         test_case = b"SSH-2.0-billsSSH_3.6.3q3\r\n"
         self.assertTrue(_conn_setup._conn_setup_version_exchange(test_case))
 
