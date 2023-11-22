b5a70e49fbced3114e501716dfb36042633b7164
Ensar Gök <ensarxgok@gmail.com>
2023-11-22 23:05:23 +0300

    Have problems with signature. will try to fix

--- test/start_server.py
+++ test/start_server.py
@@ -6,6 +6,21 @@ import hashlib
 def sha1(data):
     return hashlib.sha1(data).digest()
 
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA1
+from Crypto.PublicKey import RSA
+
+private_key = RSA.import_key(open("/home/ensargok/keys/id_rsa.pem").read())
+public_key = RSA.import_key(open("/home/ensargok/keys/id_rsa.pub").read())
+
+def sign_hash(data):
+    return pkcs1_15.new(private_key).sign(SHA1.new(data))
+
+
+def mpint(x):
+    len_x = x.bit_length() // 8 + 1
+    return len_x.to_bytes(4, byteorder="big") + x.to_bytes(len_x, byteorder="big")
+
 import os
 def sign_with_key(data):
     with open("/tmp/data", "wb") as f:
@@ -82,12 +97,15 @@ def main():
     ]
     dh_g14_sha1_prime = "".join(dh_g14_sha1_prime)
     dh_g14_sha1_prime = int(dh_g14_sha1_prime.replace(" ", ""), 16)
+    assert dh_g14_sha1_prime == 32317006071311007300338913926423828248817941241140239112842009751400741706634354222619689417363569347117901737909704191754605873209195028853758986185622153212175412514901774520270235796078236248884246189477587641105928646099411723245426622522193230540919037680524235519125679715870117001058055877651038861847280257976054903569732561526167081339361799541336476559160368317896729073178384589680639671900977202194168647225871031411336429319536193471636533209717077448227988588565369208645296636077250268955505928362751121174096972998068410554359584866583291642136218231078990999448652468262416972035911852507045361090559
     dh_g14_sha1_generator = 2
-
+    dh_g14_sha1_q = (dh_g14_sha1_prime - 1) // 2
+    
 
     # Get data from client
     dh_g14_sha1 = pyssh._core._core_classes._core_packet(client.recv(4096))
     massage_code = dh_g14_sha1.payload[0] # should be 30
+    assert massage_code == 30
     multi_precision_integer_len = int.from_bytes(dh_g14_sha1.payload[1:5], byteorder="big")
     ch_client_e = int.from_bytes(dh_g14_sha1.payload[5:5+multi_precision_integer_len], byteorder="big")
 
@@ -106,32 +124,26 @@ def main():
     second hashing operation.
     """
     y = 0x1234567890abcdef
+    assert 0 < y < dh_g14_sha1_q
     ch_server_f = pow(dh_g14_sha1_generator, y, dh_g14_sha1_prime)
     K = pow(ch_client_e, y, dh_g14_sha1_prime)
     I_C = bytes(dh_g14_sha1)
+    I_C = client_kex_init.payload
     I_S = bytes(server_kex_init)
-    K_S = b"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDghTrs2T3N3vQfyUwAmAra+pj6+ZVvRkaFyc2XqeoAIfD597U8HMeICBREOaPlC+ezEGgbyp9N1adbYWiaAfxkAN23NlTqbxsKppeD7H1wdtBdKg/aOjwHP4F8C5ebZCKzDZys9QUTvFqv6cLrsZRuGGTGVtDqWtCS70Y4mCS2TZoi7n50IVBba+C/4mHUL0uStjTlkdnGj2WtZ11F0KmUhy1anzO+7V32ucesXWMdXc+HQPFhUi3DSEfsn21EWbtweTMMx/mN0UU/372ZupPYQK4N1WJgvkcO+9+uxQA1XRMyB6GGf0N4XbmURE/zC3nLI1anWNlBluZa6e0nccqK4dzEKFaPgqnlskZkOv+5q/fcap1q7z99cwTcq9Mslyj57qFfZS1OyUe8OPdv5o7C5lo1ZH66YZ09TbvY9++q4cK5+YsQQKoD8eh3F8NIlIl9AHhH9na7xUxolMxAvvV2M5XclxL1WZ7oWW/wOQ9Ybqyn+Z+lruk8s+FljZyEq/s="
-    V_C = b"SSH-2.0-OpenSSH_for_Windows_8.1"
-    V_S = b"SSH-2.0-pySSH_0.1"
+    I_S = server_kex_init.payload
+    K_S = b"AAAAB3NzaC1yc2EAAAADAQABAAABgQDghTrs2T3N3vQfyUwAmAra+pj6+ZVvRkaFyc2XqeoAIfD597U8HMeICBREOaPlC+ezEGgbyp9N1adbYWiaAfxkAN23NlTqbxsKppeD7H1wdtBdKg/aOjwHP4F8C5ebZCKzDZys9QUTvFqv6cLrsZRuGGTGVtDqWtCS70Y4mCS2TZoi7n50IVBba+C/4mHUL0uStjTlkdnGj2WtZ11F0KmUhy1anzO+7V32ucesXWMdXc+HQPFhUi3DSEfsn21EWbtweTMMx/mN0UU/372ZupPYQK4N1WJgvkcO+9+uxQA1XRMyB6GGf0N4XbmURE/zC3nLI1anWNlBluZa6e0nccqK4dzEKFaPgqnlskZkOv+5q/fcap1q7z99cwTcq9Mslyj57qFfZS1OyUe8OPdv5o7C5lo1ZH66YZ09TbvY9++q4cK5+YsQQKoD8eh3F8NIlIl9AHhH9na7xUxolMxAvvV2M5XclxL1WZ7oWW/wOQ9Ybqyn+Z+lruk8s+FljZyEq/s="
+    K_S = base64.b64decode(K_S)
     # servers public key = K_S
-
-    len_e = ch_client_e.bit_length() // 8 + 1
-    mpint_e = len_e.to_bytes(4, byteorder="big") + ch_client_e.to_bytes(len_e, byteorder="big")
-    bytes_e = ch_client_e.to_bytes(len_e, byteorder="big")
-
-    len_f = ch_server_f.bit_length() // 8 + 1
-    mpint_f = len_f.to_bytes(4, byteorder="big") + ch_server_f.to_bytes(len_f, byteorder="big")
-    bytes_f = ch_server_f.to_bytes(len_f, byteorder="big")
-
-    len_k = K.bit_length() // 8 + 1
-    byte_k = K.to_bytes(len_k, byteorder="big")
-
+    V_C = b"SSH-2.0-OpenSSH_for_Windows_8.1"
+    V_S = b"SSH-2.0-pySSH_0.1 byEnsarGok"
+    mpint_e = mpint(ch_client_e)
+    mpint_f = mpint(ch_server_f)
+    mpint_k = mpint(K)
     # H = hash(V_C || V_S || I_C || I_S || K_S || e || f || K)
-    H = sha1(V_C + V_S + I_C + I_S + K_S + bytes_e + bytes_f + byte_k)
+    H = sha1(V_C + V_S + I_C + I_S + K_S + mpint_e + mpint_f + mpint_k)
 
     # s = sign(H)
     # sign = pyssh._core._crypto._signing._signing()
-    len_ks = len(K_S).to_bytes(4, byteorder="big")
 
     id_rsa_pub_b64 = "AAAAB3NzaC1yc2EAAAADAQABAAABgQDghTrs2T3N3vQfyUwAmAra+pj6+ZVvRkaFyc2XqeoAIfD597U8HMeICBREOaPlC+ezEGgbyp9N1adbYWiaAfxkAN23NlTqbxsKppeD7H1wdtBdKg/aOjwHP4F8C5ebZCKzDZys9QUTvFqv6cLrsZRuGGTGVtDqWtCS70Y4mCS2TZoi7n50IVBba+C/4mHUL0uStjTlkdnGj2WtZ11F0KmUhy1anzO+7V32ucesXWMdXc+HQPFhUi3DSEfsn21EWbtweTMMx/mN0UU/372ZupPYQK4N1WJgvkcO+9+uxQA1XRMyB6GGf0N4XbmURE/zC3nLI1anWNlBluZa6e0nccqK4dzEKFaPgqnlskZkOv+5q/fcap1q7z99cwTcq9Mslyj57qFfZS1OyUe8OPdv5o7C5lo1ZH66YZ09TbvY9++q4cK5+YsQQKoD8eh3F8NIlIl9AHhH9na7xUxolMxAvvV2M5XclxL1WZ7oWW/wOQ9Ybqyn+Z+lruk8s+FljZyEq/s="
     id_rsa_byte = base64.b64decode(id_rsa_pub_b64)
@@ -139,16 +151,17 @@ def main():
     
     payload = b"\x1F" + id_rsa_len + id_rsa_byte + mpint_f
 
-    # KEX host signature for rsa
+    signature1 = sign_with_key(H)
+    rsa_signature = sign_hash(H)
+    print(signature1 == rsa_signature)
+    len_s = len(rsa_signature).to_bytes(4, byteorder="big")
 
-
-    signature = sign_with_key(H)
-    len_s = len(signature).to_bytes(4, byteorder="big")
     host_sign_type = b"ssh-rsa"
     len_host_sign_type = len(host_sign_type).to_bytes(4, byteorder="big")
-    host_sign_len = len(signature) + len(host_sign_type) + 8
+
+    host_sign_len = len(rsa_signature) + len(host_sign_type) + 8
     host_sign_len = host_sign_len.to_bytes(4, "big")
-    payload += host_sign_len + len_host_sign_type + host_sign_type + len_s + signature
+    payload += host_sign_len + len_host_sign_type + host_sign_type + len_s + rsa_signature
 
     payload = pyssh._core._core_classes._core_packet._create_packet(payload)
     client.send(bytes(payload))
