e136d68106b44d415ad9b76a9808461fbe415758
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2024-06-02 00:09:19 +0300

    server: listening on 0.0.0.0 instead of localhost

--- pyssh/_core/server.py
+++ pyssh/_core/server.py
@@ -110,7 +110,7 @@ class pySSH:
         import socket
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('localhost', port))
+        sock.bind(('0.0.0.0', port))
 
         sock.listen(1)
 
